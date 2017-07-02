c6ba5d6ac6fda1df974c9b11bc901cf4d3031637
Ken Berkeley <kenberkeley@foxmail.com>
2017-07-02 21:02:19 +0800

    build/config/PATHS.js ROOT is unnecessary to expose

--- build/config/PATHS.js
+++ build/config/PATHS.js
@@ -12,7 +12,6 @@ String.prototype.join = function (target) {
 var ROOT = path.resolve(__dirname, '../..');
 
 module.exports = {
-  ROOT: ROOT,                 // 项目根目录
   DIST: ROOT.join('dist'),    // build 后输出目录
   SRC: ROOT.join('src'),      // 源码目录
   STATIC: ROOT.join('static') // 高度静态资源目录
