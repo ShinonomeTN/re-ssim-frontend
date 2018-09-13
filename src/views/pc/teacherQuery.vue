bd09a30ab295ce2a70c06518847411905777af6b
CattenLinger <lingerkong03@gmail.com>
2018-09-14 00:02:23 +0800

    Commit

--- src/views/pc/teacherQuery.vue
+++ src/views/pc/teacherQuery.vue
@@ -1,33 +1,48 @@
 <template>
-  <div>
-    <div class="page-header">
-      <h1 align="center">教师课表 <small>in {{term}}</small></h1>
-    </div>
+  <div class="container-fuild" style="margin-top: 10pt">
     <div class="row">
-      <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-        <div class="panel panel-primary">
-          <div class="panel-heading">
-            <h3>教师课表</h3>
-          </div>
-          <div class="panel-body">
-            <div class="form-group">
-              <label>教师名称</label>
-              <v-select :options="teacherList" v-model="queryForm.teacher"></v-select>
+      <!-- Term info, class choosing and exclude type choosing -->
+      <div class="col col-lg-4">
+        <mu-paper :z-depth='1'>
+
+          <mu-appbar style="width: 100%;" color="primary">
+            <div>
+              <p>教师课表</p>
             </div>
-            <div class="form-group">
-              <label>周 ({{minWeek}}-{{maxWeek}})</label>
-              <!-- <v-select :options="weekList" v-model="queryForm.week"></v-select> -->
-              <input type="number" :max="maxWeek" :min="minWeek" v-model="queryForm.week" class="form-control" :placeholder="`最小 ${minWeek} 最大 ${maxWeek}`">
+            <mu-button flat slot="right" @click="$router.go(-1)">
+              <mu-icon left value="arrow_back"></mu-icon>
+              返回
+            </mu-button>
+          </mu-appbar>
+
+          <div style="padding: 3pt 10pt 10pt 10pt">
+            <div>
+              <div style="padding:5pt 0pt; font-size: 17px">当前学期</div>
+              <mu-button full-width>{{term}}</mu-button>
             </div>
-            <div class="form-group">
-              <label>排除课程类型</label>
-              <v-select :options="courseTypeList" v-model="queryForm.excludedType" multiple></v-select>
+
+            <div>
+              <div style="padding:5pt 0pt; font-size: 17px">教师</div>
+              <mu-button full-width color="orange">某个教师</mu-button>
             </div>
           </div>
-          <div class="panel-footer">
-            <button class="btn btn-success btn-block">查询</button>
-            <button @click="$router.go(-1)" class="btn btn-block btn-default">返回</button>
-          </div>
+        </mu-paper>
+      </div>
+
+      <!-- Lesson list content -->
+      <div class="col col-lg-8">
+        <div>
+          <!-- Week range list -->
+          <week-bar ref="weekBar" class="mu-elevation-4" :max="maxWeek" :min="minWeek" :activated="[]"></week-bar>
+        </div>
+
+        <div style="margin: 10pt 5pt 0 0;">
+          <div class="view_trigger"><span>日视图</span> <mu-switch v-model="listMode"></mu-switch> <span>周视图</span></div>
+        </div>
+
+        <div style="margin-top: 10pt">
+          <lesson-list v-if="listMode" :data="queryResult"></lesson-list>
+          <lesson-week-page v-else :data="queryResult"></lesson-week-page>
         </div>
       </div>
     </div>
@@ -37,6 +52,10 @@
 <script>
 import Utils from "@/commons/utils";
 
+import WeekBar from "@/components/weekBar";
+import LessonList from "./classQuery/lessonList";
+import LessonWeekPage from "./classQuery/lessonWeekPage";
+
 export default {
   name: "pc-teacher-query",
 
@@ -44,19 +63,23 @@ export default {
     term: String
   },
 
+  components: {
+    WeekBar,
+    LessonList,
+    LessonWeekPage
+  },
+
   data() {
     return {
+      listMode: false,
+
       teacherList: [],
       courseTypeList: [],
 
       maxWeek: 0,
       minWeek: 0,
 
-      queryForm: {
-        teacher: "",
-        week: null,
-        excludedType: null
-      }
+      queryResult: {}
     };
   },
 
@@ -68,10 +91,6 @@ export default {
     Utils.newRequest(`/api/term/${this.term}?weekRange`).then(response => {
       this.maxWeek = response.data.max;
       this.minWeek = response.data.min;
-    });
-
-    Utils.newRequest(`/api/term/${this.term}?classType`).then(response => {
-      this.courseTypeList = response.data.classTypes;
     });
   }
 };
